f3fb1ccc9f0edc6bb84fa085f9d5b8324dc67e01
juarezdanny522-tech <juarezdanny522@gmail.com>
2026-06-22 11:31:11 -0600

    Añadir DOM
    Añadí elementos de DOM, unas variables constantes y listo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // --- ESTADO DE LA APLICACIÓN ---
+
     let carrito = [];
     let filtroActual = "todos";
     let busquedaActual = "";
+    
+    const secciones = document.querySelectorAll(".pantalla");
+    const enlaces = document.querySelectorAll("[data-seccion]");
+    const menuPrincipal = document.getElementById("menuPrincipal");
+    const listaProductos = document.getElementById("listaProductos");
+    const buscador = document.getElementById("buscador");
+    const contadorCarrito = document.getElementById("contadorCarrito");
+    const listaCarrito = document.getElementById("listaCarrito");
+    const totalCarrito = document.getElementById("totalCarrito");
+    const pestañasFiltro = document.querySelectorAll(".filter-tabs button, .category-card");
